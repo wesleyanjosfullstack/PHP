a97533167906330a17d215b36c743d11448a79b8
wesleyanjosfullstack <wesleyanjosfullstack@gmail.com>
2024-11-30 15:30:00 -0300

    Mais arquivos php com exercicios

---
+++ Aula06/referencia.php
@@ -0,0 +1,7 @@
+<?php
+$a = 3;
+$b = $a;
+$b += 5;
+echo 'A variável A vale '. $a .'<br>';
+echo 'A variável B vale '. $b .'<br>';
+?>
---
+++ Aula06/variaveisvariaveis.php
@@ -0,0 +1,6 @@
+<?php
+$x = 'abc';
+$$x = 'def';
+echo 'O conteúdo da variável X e '. $x . '<br>';
+echo 'A variável criada recebeu o valor '. $abc .'<br>';
+?>
