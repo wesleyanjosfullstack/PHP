75923d57f9caa06716b0ad62557042945818e6a2
wesleyanjosfullstack <wesleyanjosfullstack@gmail.com>
2024-11-30 13:48:17 -0300

    Alterando arquivo e adicionado operadores.php

--- Aula05/index.php
+++ Aula05/index.php
@@ -3,4 +3,4 @@ $n1 = 3;
 $n2 = 2;
 $s = $n1 + $n2;
 echo 'A soma de '.$n1.' e '.$n2.' é igual '.$s;
-?> 
+?>
---
+++ Aula05/operadores.php
@@ -0,0 +1,10 @@
+<?php
+$n1 = 3;
+$n2 = 2;
+
+echo 'A soma vale '. ($n1 + $n2) .'<br>';
+echo 'A subtração vale '. ($n1 - $n2) .'<br>';
+echo 'A multiplicação vale '. ($n1 * $n2) .'<br>';
+echo 'A divisão vale '. ($n1 / $n2) .'<br>';
+echo 'A modulo vale '. ($n1 % $n2) .'<br>';
+?>
