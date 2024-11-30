2a3dee74361eff724eabd07bb6198abb539af6b9
wesleyanjosfullstack <wesleyanjosfullstack@gmail.com>
2024-11-30 14:00:02 -0300

    Alterado arquivo operadores.php para aceitar valores por url

--- Aula05/operadores.php
+++ Aula05/operadores.php
@@ -1,10 +1,14 @@
 <?php
-$n1 = 3;
-$n2 = 2;
+// http://localhost:8000/operadores.php?a=5&b=8
+$n1 = $_GET['a'];
+$n2 = $_GET['b'];
+echo 'Valores recebidos: '. $n1 . ' e ' . $n2 .'<br>';
+$m = ($n1 + $n2) / 2;
 
 echo 'A soma vale '. ($n1 + $n2) .'<br>';
 echo 'A subtração vale '. ($n1 - $n2) .'<br>';
 echo 'A multiplicação vale '. ($n1 * $n2) .'<br>';
 echo 'A divisão vale '. ($n1 / $n2) .'<br>';
 echo 'A modulo vale '. ($n1 % $n2) .'<br>';
+echo 'A média vale '. $m .'<br>';
 ?>
