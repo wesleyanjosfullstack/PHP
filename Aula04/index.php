f9f50fee5a84cfe1f8350545a71cd9756d3b64d7
wesleyanjosfullstack <wesleyanjosfullstack@gmail.com>
2024-11-30 13:08:45 -0300

    Alterando arquivo php sobre variaveis
    Aula 04 sobre variáveis

--- Aula04/index.php
+++ Aula04/index.php
@@ -1,4 +1,12 @@
 <?php
+$n = 5;
 $name = 'Wesley dos Anjos';
-echo mb_strtoupper($name);
+echo $name;
+echo '<br>'.$n;
+$n = 4.5;
+echo '<br>'.$n;
+$age = 27;
+
+echo '<br>'.$name.' tem '.$age.' anos !';
+echo "<br>$name tem $age anos !";
 ?>
