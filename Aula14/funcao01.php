78809972ce7781cd8033103ebd44bfa19974970d
wesleyanjosfullstack <wesleyanjosfullstack@gmail.com>
2024-12-03 13:50:13 -0300

    Aula funcao em php
    Aula 14 Funções

---
+++ Aula14/funcao01.php
@@ -0,0 +1,12 @@
+<?php
+    function soma($a, $b) {
+        $s = $a + $b;
+        echo '<p>A soma vale '. $s .'</p>';
+    }
+
+    soma(3, 5);
+    soma(8, 2);
+    $x = 9;
+    $y = 15;
+    soma($x, $y);
+?>
